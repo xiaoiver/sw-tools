5ad260fc450583af017afda063ed6f505e4caf4f
xiaoiver <pyqiverson@gmail.com>
2018-07-02 16:07:43 +0800

    feat: add sw-toolbox example

---
+++ examples/sw-toolbox/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PUBLIC_PATH = '/sw-tools/examples/sw-toolbox/dist/';
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/index'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:8].js',
+        publicPath: PUBLIC_PATH,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 100,
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('index.css'),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        }),
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'sw-tools',
+            dontCacheBustUrlsMatching: /\.\w{8}\./,
+            filename: 'service-worker.js',
+            minify: false,
+            navigateFallback: PUBLIC_PATH + 'index.html',
+            staticFileGlobsIgnorePatterns: [/\.map$/, /\.png$/],
+            runtimeCaching: [{
+                urlPattern: '/.*\.png$',
+                handler: 'networkFirst'
+            }]
+        })
+    ]
+}
